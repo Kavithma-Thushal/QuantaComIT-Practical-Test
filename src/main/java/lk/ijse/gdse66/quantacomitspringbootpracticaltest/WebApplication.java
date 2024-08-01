d895ec2038388ef1bf11bf9c013eed108e5860d5
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-01 12:22:13 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse66/quantacomitspringbootpracticaltest/WebApplication.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse66.quantacomitspringbootpracticaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
